694d0cfdb5535afe892ba7a2f795b001b221ba41
Ayodeji <ayodeji_oloruntoba@yahoo.com>
2024-01-22 09:20:17 -0800

    A function sorting an array of integers in ascending order using the Quick sort algorithm

---
+++ 3-quick_sort.c
@@ -0,0 +1,80 @@
+#include "sort.h"
+/**
+ * swap_array - function to swap array of integers
+ * @array: pointer to the array of integers
+ * @size: size of array
+ * @a: pointer to first element in the array to be swapped
+ * @b: pointer to second element in the array to be swapped
+ * Return: Nothing
+ */
+void swap_array(int *array, size_t size, int *a, int *b)
+{
+	if (*a != *b)
+	{
+		*a = *a + *b;
+		*b = *a - *b;
+		*a = *a - *b;
+		print_array((const int *)array, size);
+	}
+}
+/**
+ * quick_sort - function to sort the array of integers in ascending order
+ * @array: pointer to the array of integers
+ * @size: size of array
+ *
+ * Return: Nothing
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size <= 1)
+	{
+		return;
+	}
+	quicksorter(array, size, 0, size - 1);
+}
+/**
+ * Lomuto_partitioner - function to partition the array of integers
+ * @array: pointer to the array of integers
+ * @size: size of array
+ * @f_idx: starting index of partition
+ * @l_idx: ending index of partition
+ *
+ * Return: pivot index
+ */
+size_t Lomuto_partitioner(int *array, size_t size, int f_idx, int l_idx)
+{
+	int piv_idx;
+	int piv_val;
+
+	piv_val = array[l_idx];
+	for (piv_idx = f_idx; f_idx < l_idx; f_idx++)
+	{
+		if (array[f_idx] < piv_val)
+		{
+			swap_array(array, size, &array[f_idx], &array[piv_idx]);
+			piv_idx++;
+		}
+	}
+	swap_array(array, size, &array[piv_idx], &array[l_idx]);
+	return (piv_idx);
+}
+/**
+ * quicksorter - Sorts the array of integers in ascending order
+ * @array: pointer to the array of integers
+ * @size: size of array
+ * @start_idx: start index of the array
+ * @end_idx: end index of the array
+ *
+ * Return: Nothing
+ */
+void quicksorter(int *array, size_t size, int start_idx, int end_idx)
+{
+	size_t piv_idx;
+
+	if (start_idx < end_idx)
+	{
+		piv_idx = Lomuto_partitioner(array, size, start_idx, end_idx);
+		quicksorter(array, size, start_idx, piv_idx - 1);
+		quicksorter(array, size, piv_idx + 1, end_idx);
+	}
+}
--- sort.h
+++ sort.h
@@ -16,10 +16,14 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void swap_array(int *array, size_t size, int *a, int *b);
+size_t Lomuto_partitioner(int *array, size_t size, int low, int high);
+void quicksorter(int *array, size_t size, int low, int high);
 #endif
